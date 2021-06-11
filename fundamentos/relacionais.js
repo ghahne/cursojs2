5ebe4d33982b1e76658caa1f8cf64d04c37946dd
Guilherme Hahne <Guilherme Hahne>
2021-06-11 14:16:51 -0300

    teste

--- fundamentos/relacionais.js
+++ fundamentos/relacionais.js
@@ -13,5 +13,6 @@ const d2 = new Date(0)
 console.log('09)', d1 === d2)
 console.log('10)', d1 == d2)
 console.log('11)', d1.getTime() == d2.getTime())
+
 console.log('12)', undefined == null)
 console.log('12)', undefined === null)
