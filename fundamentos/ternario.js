66f01302d4c008019d70904c22e81813f6f1e87d
Guilherme Hahne <Guilherme Hahne>
2021-07-12 18:27:44 -0300

    12/07 estudo

---
+++ fundamentos/ternario.js
@@ -0,0 +1,5 @@
+const resultado = nota => nota >= 7 ? 'aprovado' : 'reprovado' // operador ternário
+console.log(resultado(7.1))
+console.log(resultado(6.7))
+
+const satus = nota >= 7 ? 'Aprovado' : 'Reprovado'
---
+++ fundamentos/unarios.js
@@ -0,0 +1,12 @@
+let num1 = 1
+let num2 = 2
+
+num1++ // "++" acrescenta uma unidade a operação
+console.log(num1)
+
+--num1 // "--" subtrai uma unidade da operação
+console.log(num1)
+
+console.log(++num1 === num2--) // Código complexo, não ideal
+console.log(num1 === num2)
+
