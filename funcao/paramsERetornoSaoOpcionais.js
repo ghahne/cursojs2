59817fdff7c7a9c1bfce039cf7a9192d7ca331f4
Guilherme Hahne <ghahne@outlook.com>
2022-02-10 10:50:45 -0300

    Parametros e Retornos s

---
+++ funcao/paramsERetornoSaoOpcionais.js
@@ -0,0 +1,12 @@
+function area2 (largura, altura) {
+    const area = largura * altura
+    if (area > 20) {
+        console.log (`Valor acima do permitido: ${area}m2.`) // Se você jogar um Return aqui ao inves de Console.log, terá apenas a string concatenada e não o undef
+    } else {
+        return area
+    }     
+}
+
+
+console.log(area2(4, 5))
+console.log(area2(5, 5))
