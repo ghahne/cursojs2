6b8ee10bd0a1917c1ac5309b4745fd1ce02de695
Guilherme Hahne <ghahne@outlook.com>
2022-02-10 13:08:26 -0300

    commit de teste após alteração de função e etc

--- funcao/paramsERetornoSaoOpcionais.js
+++ funcao/paramsERetornoSaoOpcionais.js
@@ -1,4 +1,4 @@
-function area2 (largura, altura) {
+function area (largura, altura) {
     const area = largura * altura
     if (area > 20) {
         console.log (`Valor acima do permitido: ${area}m2.`) // Se você jogar um Return aqui ao inves de Console.log, terá apenas a string concatenada e não o undef
@@ -8,5 +8,6 @@ function area2 (largura, altura) {
 }
 
 
-console.log(area2(4, 5))
-console.log(area2(5, 5))
+console.log(area(4, 5))
+console.log(area(5, 5))
+
