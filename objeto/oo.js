48bdc538189900764609386ca218e275f8f78614
Guilherme Hahne <gui-hahne@hotmail.com>
2022-12-02 11:27:12 -0300

    OO

---
+++ objeto/oo.js
@@ -0,0 +1,23 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural (exemplo de linguagem: C)
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+// acessariámos a função processamento acima da seguinte forma
+objeto.processamento()
+
+// Principios importantes
+// .1 abstração
+// .2 encapsulamento (esconder detalhes de implementação, não deixar exposto os detalhes do objeto para não existir alto nível de ACOPLAMENTO e não gerar dependência)
+// .3 herança (prototype)
+// .4 polimorfismo (várias formas, esse conceito faz mais sentido em linguagens tipadas)
