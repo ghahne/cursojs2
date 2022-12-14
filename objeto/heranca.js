e155b57242b516e7dc0cc2d554587fc12ec0fc1e
Guilherme Hahne <gui-hahne@hotmail.com>
2022-12-14 11:00:08 -0300

    heranca em JS

---
+++ objeto/heranca.js
@@ -0,0 +1,22 @@
+const ferrari = {
+    modelo: 'f40',
+    velMax: 324,
+}
+
+const volvo = {
+    modelo: 'v40',
+    velMax: 200
+}
+
+console.log(ferrari.__proto__)
+console.log(ferrari.__proto__ === Object.prototype)
+console.log(volvo.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__)
+console.log(Object.prototype.__proto__ === null)
+
+function meuObj () {
+
+}
+
+console.log(typeof Object, typeof meuObj)
+console.log(Object.prototype, meuObj.prototype)
